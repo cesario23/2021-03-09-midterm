11c088d39303157d092cc3b0b97c5baee9b36f76
cesar juarez <cesar.juarez@codeimmersives.com>
2021-03-10 11:31:22 -0800

    midterm

--- main.js
+++ main.js
@@ -1,6 +1,64 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function ageToAbilities (age){
+    if (age < 16 ){
+        return "You can't drive."
+    }else if (age >= 16 && age <= 17){
+        return "You can drive but not vote."
+    }else if (age >= 18 && age <= 24){
+        return "You can vote but not rent a car."
+    }else {
+        return 'You can do pretty much anything.'
+    }
+}
+
+function oddIndices (array){
+    let output = [];
+    for (let i = 0; i < array.length; i++){
+    if (array [i] % 2 !== 0){
+       }
+    }
+    return output;
+}
+
+function numOddValues (array){
+    let sumOddValues = 0;
+ for (const char of array){
+     if (char % 2 !== 0){ 
+         sumOddValues += char;
+     }
+ }
+ return sumOddValues;
+}
+
+
+function averageStringLength (arry){
+let averageLengthStr = 0;
+  for (const char of arry){
+      const strLength = arry.length ();
+    averageLengthStr = char / strLength 
+  }
+  return averageLengthStr;
+ }
+
+
+ function firstPunctuationIndex (str){
+for (let i =0; i < str.length ; i++ ){
+    
+    if (i === '.' || i === '!' || i === "?"){
+        break;
+    
+    }
+    
+}
+ }
+
+ function getPlace (list, grade){
+
+ 
+ }
+
 
 
 
